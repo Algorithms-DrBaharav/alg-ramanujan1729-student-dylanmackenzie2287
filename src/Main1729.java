822585562ac894d2f381a3f677b9329b2645bd0e
dylanmackenzie2287 <dylanmitsuo@gmail.com>
2018-08-23 11:37:32 -0700

    fin

--- src/Main1729.java
+++ src/Main1729.java
@@ -1,4 +1,4 @@
-
+import java.lang.*;
 /**
  *
  * @author YourNameHere
@@ -8,6 +8,19 @@
  * 
  */
 
+ 
 
+class Main1729{
+    
+    
+    public static void main(String[] args) {
+        double num = 1729;
+        for(int i =0; i< Math.cbrt(num); i++)
+            for(int y =1; y<i; y++)
+                if((i*i*i + y*y*y)== 1729)
+                    System.out.println(i + " and " + y);
+            
+    }
 
+}
 // Your work here. Just carete one class and one main function.
